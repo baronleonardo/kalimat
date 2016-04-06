56b4886ddd55764f4f09f163c7e2a1ca0ea1daf5
Adham Zahran <adham.zahran@hotmail.com>
2016-04-06 05:43:18 +0200

    created an index that connects all of the pages as well as windows downloads

---
+++ كلمات بالأمثلة - كلِمَات_files/index.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 5:52:44 Jan 18, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:37:48 Apr 5, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '/w/skins';
+
+/* MediaWiki:Common.js */
+/* الجافاسكريبت الموضوع هنا سيتم تحميله لكل المستخدمين مع كل تحميل للصفحة. */
+
+/* MediaWiki:Monobook.js */
+/* أي جافاسكريبت هنا سيتم تحميلها للمستخدمين الذين يستعملون واجهة مونوبوك */
